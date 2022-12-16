e107c9fb7e87199f891095d7826010a0af34f83b
jitendrakumarh7 <120735188+jitendrakumarh7@users.noreply.github.com>
2022-12-16 22:33:20 +0530

    #include<stdio.h> int main() {     int r,n,s=0,p=1;     scanf("%d",&n);     while(n!=0)     {         r=n%10;         p=p*r;         s=s+r;         n=n/10;     }     printf("%d",p-s); }

---
+++ Subtract_the_Product_and_Sum_of_Digits_of_an_Integer.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int r,n,s=0,p=1;
+    scanf("%d",&n);
+    while(n!=0)
+    {
+        r=n%10;
+        p=p*r;
+        s=s+r;
+        n=n/10;
+    }
+    printf("%d",p-s);
+}
