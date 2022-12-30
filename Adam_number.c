4471195119b4c79cb8f95af9744bcfe85fbeec40
jitendrakumarh7 <120735188+jitendrakumarh7@users.noreply.github.com>
2022-12-30 22:58:30 +0530

    #include<stdio.h> int main() {     int n,p,a,r,r1,s=0,s1=0;     scanf("%d",&n);     a=n*n;     while(n!=0)     {         r=n%10;         s=s*10+r;         n=n/10;     }     p=s*s;     while(p!=0)     {         r1=p%10;         s1=s1*10+r1;         p=p/10;     }     if(a==s1)     {         printf("True");     }     else     {         printf("False");     } }

--- Adam_number.c
+++ Adam_number.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    int n,a,r,s=0,a1,s1=0,r1;
+    int n,p,a,r,r1,s=0,s1=0;
     scanf("%d",&n);
     a=n*n;
     while(n!=0)
@@ -10,21 +10,19 @@ int main()
         s=s*10+r;
         n=n/10;
     }
-    a1=s*s;
-    while(a1!=0)
+    p=s*s;
+    while(p!=0)
     {
-        r1=a1%10;
+        r1=p%10;
         s1=s1*10+r1;
-        a1=a1/10;
+        p=p/10;
     }
     if(a==s1)
     {
         printf("True");
     }
     else
-{
-    printf("False");
-}
-    
-    
+    {
+        printf("False");
+    }
 }
