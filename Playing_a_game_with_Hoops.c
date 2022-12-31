4f99ddeb8a9b2893046db7c5517455fad7effb0d
jitendrakumarh7 <120735188+jitendrakumarh7@users.noreply.github.com>
2022-12-31 11:27:39 +0530

    #include<stdio.h> int main() {     int i,n;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         int a;         scanf("%d",&a);         if(a%2==0){             printf("%d ",a/2);         }         else         {             printf("%d ",(a/2)+1);         }     } }

---
+++ Playing_a_game_with_Hoops.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int i,n;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        int a;
+        scanf("%d",&a);
+        if(a%2==0){
+            printf("%d
+",a/2);
+        }
+        else
+        {
+            printf("%d
+",(a/2)+1);
+        }
+    }
+}
