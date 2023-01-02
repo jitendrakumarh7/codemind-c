dd40c6ebfb8731fd40d91663402745c5ea750ae2
jitendrakumarh7 <120735188+jitendrakumarh7@users.noreply.github.com>
2023-01-03 00:18:22 +0530

    #include<stdio.h> int main() {     int n,i;     scanf("%d",&n);     int x[n];     for(i=0;i<n;i++)     {         scanf("%d",&x[i]);     }     for(i=0;i<n;i++)     {         if(x[i]%2!=0)         {             printf("%d ",x[i]);         }     }     for(i=0;i<n;i++)     {         if(x[i]%2==0)         {             printf("%d ",x[i]);         }     }
    }

---
+++ array_of_add_and_even_elements.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    int x[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&x[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(x[i]%2!=0)
+        {
+            printf("%d ",x[i]);
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        if(x[i]%2==0)
+        {
+            printf("%d ",x[i]);
+        }
+    }
+    
+    
+    
+}
