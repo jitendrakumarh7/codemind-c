732316a25f92a466c429d0fcffc510dfd9a9b5d1
jitendrakumarh7 <120735188+jitendrakumarh7@users.noreply.github.com>
2023-05-27 23:23:38 +0530

    #include<stdio.h> int main() {     int a,b;     scanf("%d%d",&a,&b);     int min=a<b?a:b;     while(min)     {         if(a%min==0&&b%min==0)         {             printf("%d",min);             break;         }         min--;     } }

--- GCD_or_HCF.c
+++ GCD_or_HCF.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 int main()
 {
-    int a,b,min,i;
+    int a,b;
     scanf("%d%d",&a,&b);
-    min=a<b?a:b;
-   
-    for(i=min;;i--)
+    int min=a<b?a:b;
+    while(min)
     {
-        if(a%i==0&&b%i==0)
+        if(a%min==0&&b%min==0)
         {
-            printf("%d",i);
+            printf("%d",min);
             break;
         }
+        min--;
     }
 }
