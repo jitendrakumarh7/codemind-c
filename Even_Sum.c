de9e560a4f05e31d9066b4a5c920d12643b748f1
jitendrakumarh7 <120735188+jitendrakumarh7@users.noreply.github.com>
2023-06-06 23:21:52 +0530

    #include<stdio.h> int main() {     int n,i,s=0;     scanf("%d",&n);     int x[n];     for(i=0;i<n;i++)     {         scanf("%d",&x[i]);     }     for(i=0;i<n;i++)     {         if(x[i]%2==0)         {             s=s+x[i];         }     }     printf("%d",s); }

---
+++ Even_Sum.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,s=0;
+    scanf("%d",&n);
+    int x[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&x[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(x[i]%2==0)
+        {
+            s=s+x[i];
+        }
+    }
+    printf("%d",s);
+}
